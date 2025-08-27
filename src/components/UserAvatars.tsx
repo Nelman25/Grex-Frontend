8003e7bba709d791de826a71bf316121eb01f30a
Jonel Villaver <jonelvillaver735@gmail.com>
2025-08-28 04:29:30 +0800

    feat(ui): add UserAvatar component
    - displays multiple avatars of users

---
+++ src/components/UserAvatars.tsx
@@ -0,0 +1,24 @@
+type Props = {
+  images: string[];
+};
+
+// TODO: improve this component by adding hover card to each avatar
+
+export default function UserAvatars({ images }: Props) {
+  const length = images.length;
+
+  return (
+    <div className="relative flex items-center">
+      {images.slice(0, 5).map((image) => (
+        <img
+          key={image}
+          src={image}
+          className="size-7 rounded-full border-2 border-dark-muted -ml-2 first:ml-0"
+        />
+      ))}
+      {length > 5 && (
+        <p className="text-sm ml-2 text-dark-subtle">+ {length - 5} more</p>
+      )}
+    </div>
+  );
+}
