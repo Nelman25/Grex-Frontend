693f098e7f9b85cf3acfdc9f23ec4447a12a67c1
Jonel Villaver <jonelvillaver735@gmail.com>
2025-09-06 04:30:23 +0800

    types(chat): update chat message interfaces and unify shapes

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -4,42 +4,34 @@ export interface OutgoingChatMessage {
   reply_to: number | null;
 }
 
-export interface IncomingChatMessage {
+export interface IncomingChatMessage extends OutgoingChatMessage {
   message_id: number;
   workspace_id: number;
   sender_id: number;
-  type: "text" | "file" | "poll";
-  content: string;
-  reply_to: number | null;
-  sent_at: string;
-}
-
-export interface ChatMessage {
-  message_id: number;
-  workspace_id: number;
-  sender_id: number;
+  avatar: string | null; // temp optional because of naming conflicts
   nickname: string;
-  profile_picture: string | null;
-  message_type: "text" | "file" | "poll";
-  reply_to: number | null;
   sent_at: string;
-  content: string;
-  file_url: string | null;
-  file_type: string | null;
-  question: string | null;
 }
 
-export interface PendingChatMessage {
+// change message_id to temp_id
+export interface PendingChatMessage
+  extends Omit<IncomingChatMessage, "message_id"> {
   temp_id: string;
-  workspace_id: number;
-  sender_id: number;
-  nickname: string;
-  profile_picture: string | null;
-  message_type: "text" | "file" | "poll";
-  reply_to: number | null;
-  sent_at: string; // new Date().toISOString()
-  content: string;
-  status: "pending" | "failed"; // UI state
 }
 
-export type AnyChatMessage = ChatMessage | PendingChatMessage;
+// All these fields are temporarily set to null because we are focusing on text messages first, but these are
+// part of the response shape that's why these are here
+export interface MessageHistoryItem
+  extends Omit<IncomingChatMessage, "avatar" | "type"> {
+  // removing avatar and type here temporarily because of naming mismatch
+  profile_picture: string | null;
+  message_type: string;
+  file_url?: string | null;
+  file_type?: string | null;
+  question?: string | null;
+}
+
+export type ChatMessage =
+  | IncomingChatMessage
+  | MessageHistoryItem
+  | PendingChatMessage;
