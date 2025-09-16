853d22b37b5423d614ae9c3ae3ff43847ba91a01
Jonel Villaver <jonelvillaver735@gmail.com>
2025-09-16 18:54:49 +0800

    refactor(hooks): rename useUserWorkspaces to useFetchAllWorkspacesQuery

--- src/features/workspace/hooks/queries/useFetchAllWorkspacesQuery.ts
+++ src/features/workspace/hooks/queries/useFetchAllWorkspacesQuery.ts
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getWorkspaces } from "../../api/workspaceApi";
 import type { UserWorkspacesResponse } from "@/types/project";
 
-export const useUserWorkspaces = (user_id: number | undefined) => {
+export const useFetchAllWorkspacesQuery = (user_id: number | undefined) => {
   if (!user_id) throw new Error("Invalid user id");
 
   return useQuery<UserWorkspacesResponse[], Error>({
-    queryKey: ["workspaces"],
+    queryKey: ["workspaces", { user_id }],
     queryFn: () => getWorkspaces(user_id),
   });
 };
