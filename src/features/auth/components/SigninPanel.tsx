4a79b672f94c9f6b70dbfdfa54bf364b2a63fb89
Jonel Villaver <jonelvillaver735@gmail.com>
2025-11-08 11:11:04 +0800

    fix(signin): remove incorrect account creation disclaimer

--- src/features/auth/components/SigninPanel.tsx
+++ src/features/auth/components/SigninPanel.tsx
@@ -4,7 +4,7 @@ import SocialButtonsContainer from "./SocialButtonsContainer";
 
 export default function SigninPanel() {
   return (
-    <div className="flex-1 bg-gradient-to-br from-[#F2F2F2]/15 to-[#7E7E7E]/15 rounded-lg py-6 px-4 mt-4 lg:py-8 lg:px-8">
+    <div className="flex-1 flex flex-col justify-center bg-gradient-to-br from-[#F2F2F2]/15 to-[#7E7E7E]/15 rounded-lg py-6 px-4 mt-4 lg:p-8">
       <SocialButtonsContainer signin />
 
       <div className="flex space-x-4 items-center my-4">
@@ -16,12 +16,6 @@ export default function SigninPanel() {
       <SigninForm />
 
       <div className="flex flex-col space-y-6 items-center justify-center">
-        <p className="text-dark-subtle text-center">
-          By creating an account, you agree to Grex's{" "}
-          <Link to="/">
-            <span className="text-dark-text font-semibold underline">Terms of Service and Privacy</span>
-          </Link>
-        </p>
         <p className="text-center text-dark-text">
           Don't have an account?{" "}
           <Link to="/auth/signup">
